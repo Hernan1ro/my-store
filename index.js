80a8234f325cc7c1cfeabb740873735d94688881
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-15 14:02:17 -0500

    routing with express

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const port = 3005;
 app.get('/', (request, response) => {
   response.send('Hola mi server en express');
 });
-
+app.get('/productos', (req, res) => {
+  res.send('Hola soy producto section');
+});
+app.get('/nigga', (req, res) => {
+  res.json({
+    name: 'Hola nigaa',
+    appelido: 'escrito mal papau',
+  });
+});
 app.listen(port, () => {
   console.log('Mi port' + port);
 });
