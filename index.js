3d3e59ea8f6d170b09402ad1151bc7589e3f97f2
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-15 17:47:03 -0500

    Adding categoriy and user routes to index

--- index.js
+++ index.js
@@ -13,27 +13,4 @@ app.listen(port, () => {
   console.log('Mi port' + port);
 });
 
-// Parametros tipo query para obtener datos
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    });
-  } else {
-    res.send('No hay parametros');
-  }
-});
-
-// Single Responsability Principle
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-  });
-});
-
 routerApi(app);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const productsRouter = require('./productsRouter');
-// const usersRouter = require('./usersRouter');
+const usersRouter = require('./usersRouter');
+const categoriesRouter = require('./categoriesRouter');
+
 function routerApi(app) {
   app.use('/products', productsRouter);
-  // app.use('/users', usersRouter);
+  app.use('/categories', categoriesRouter);
+  app.use('/users', usersRouter);
 }
 
 module.exports = routerApi;
