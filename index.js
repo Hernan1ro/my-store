9acac3e1b648b8e88eb881fabc3e1417f034ca2f
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-15 15:20:51 -0500

    creating end points with multiple variables

--- index.js
+++ index.js
@@ -6,15 +6,26 @@ const port = 3005;
 app.get('/', (request, response) => {
   response.send('Hola mi server en express');
 });
-app.get('/productos', (req, res) => {
-  res.send('Hola soy producto section');
+app.get('/products', (req, res) => {
+  res.json([
+    { name: 'play station 2', price: 20000 },
+    { name: 'play station 4', price: 30000 },
+    { name: 'play station 5', price: 50000 },
+  ]);
 });
-app.get('/nigga', (req, res) => {
+app.get('/products/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({ id, name: 'play station 2', price: 20000 });
+});
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
   res.json({
-    name: 'Hola nigaa',
-    appelido: 'escrito mal papau',
+    categoryId,
+    productId,
   });
 });
+
 app.listen(port, () => {
   console.log('Mi port' + port);
 });
