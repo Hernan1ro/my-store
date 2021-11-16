b65216050e6ffa8faf2f5baa6878dd3dcda7098f
Hernán D. Mercado <hernandmf@gmail.com>
2021-11-15 23:53:21 -0500

    handling status codes

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -26,13 +26,19 @@ router.get('/filter', (rep, res) => {
 // Get elements
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({ id, name: 'play station 2', price: 20000 });
+  if (id === '999') {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  } else {
+    res.status(200).json({ id, name: 'play station 2', price: 20000 });
+  }
 });
 
 // Post element
 router.post('/', (req, res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: 'created',
     data: body,
   });
